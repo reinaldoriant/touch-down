e759995659eb3b558aa849cf9c5ed666f839c1cf
Reinaldo <reinaldo.riant@kompas.com>
2021-07-08 09:44:01 +0700

    Initial Commit

---
+++ touchDown/touchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  touchDownApp.swift
+//  touchDown
+//
+//  Created by TI Digital on 08/07/21.
+//
+
+import SwiftUI
+
+@main
+struct touchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
