db30b191c3ced1241be3ca65dc5fa8eec670c740
Reinaldo <reinaldo.riant@kompas.com>
2021-07-08 19:47:57 +0700

    ! modify button

--- touchDown/View/Home/NavigationBarView.swift
+++ touchDown/View/Home/NavigationBarView.swift
@@ -20,6 +20,19 @@ struct NavigationBarView: View {
                     .foregroundColor(.black)
             })//: Button
             Spacer()
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                ZStack {
+                    Image(systemName: "cart")
+                        .font(.title)
+                        .foregroundColor(.black)
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 14, height: 14, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .offset(x: 13, y: -10)
+                }
+                
+            })
         }//: HStack
     }
 }
@@ -27,7 +40,8 @@ struct NavigationBarView: View {
 struct NavigationBarView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationBarView()
-            .padding()
             .previewLayout(.sizeThatFits)
+            .padding()
+        
     }
 }
