1e659b722ed4d5b02a6693c89ffd6cc39a277b0e
Reinaldo <reinaldo.riant@kompas.com>
2021-07-09 09:35:13 +0700

    + add categories model

---
+++ touchDown/Model/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  touchDown
+//
+//  Created by TI Digital on 09/07/21.
+//
+
+import Foundation
+
+struct Category: Codable, Identifiable{
+    let id: Int
+    let name: String
+    let image:String
+}
